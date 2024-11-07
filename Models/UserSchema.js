b04d537d801042749f73a024d1e5941104659adb
Moeez-Muslim <mail2moeezmuslim@gmail.com>
2024-11-07 22:38:33 +0500

    phone number required set to false

--- Models/UserSchema.js
+++ Models/UserSchema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  phoneNumber: { type: String, required: true },
+  phoneNumber: { type: String},
   password: { type: String, required: false },
   isAdmin: { type: Boolean, default: false },  
 });
